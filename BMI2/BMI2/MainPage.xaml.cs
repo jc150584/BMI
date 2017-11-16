b4d9b56d186cefc54c6a2b7ceb433f3ec2afe2cc
有希奈 澤田 <150584@jc-21.jp>
2017-11-16 14:28:52 +0900

    初回

--- BMI2/BMI2/MainPage.xaml.cs
+++ BMI2/BMI2/MainPage.xaml.cs
@@ -13,5 +13,48 @@ namespace BMI2
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double w = Double.Parse(Weight.Text);
+            Double h = Double.Parse(Height.Text);
+
+            h /= 100;
+
+            double bmi = w / h / h;
+
+            double code = System.Math.Pow(10, 2);
+            bmi = bmi > 0 ? System.Math.Floor(bmi * code) / code :
+                System.Math.Ceiling(bmi * code) / code;
+
+            string bmit = "";
+
+            if (bmi < 18.5)
+            {
+                bmit = "低体重（痩せ型）";
+            }
+            else if (bmi < 25)
+            {
+                bmit = "普通体重";
+            }
+            else if (bmi < 30)
+            {
+                bmit = "肥満（1度）";
+            }
+            else if (bmi < 35)
+            {
+                bmit = "肥満（2度）";
+            }
+            else if (bmi < 40)
+            {
+                bmit = "肥満（3度）";
+            }
+            else
+            {
+                bmit = "肥満（4度）";
+            }
+
+            Bmi.Text = string.Format("BMI {0} {1}", bmi, bmit);
+        }
     }
 }
